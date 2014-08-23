c0608fd4b635ad0341341e4efce8d28e08b5545b
Eli <coloredflare@gmail.com>
2014-08-23 12:25:26 +0300

    Add awesome script This awesome script auto saves your game progress, wow

---
+++ script.js
@@ -0,0 +1,39 @@
+var ImaginaryWorld = {
+	player: {
+		name: "Player",
+		hp: 100
+	},
+	
+	savingLoop: function() {
+		setInterval(function() {
+			ImaginaryWorld.save();
+		}, 5000);
+	},
+	
+	start: function() {
+		if(this.getItem("saved")) {
+			alert("saved");
+			this.load();
+		}
+		this.savingLoop();
+	},
+	
+	save: function() {
+		this.setItem("hp", this.player.hp);
+		this.setItem("name", this.player.name);
+		this.setItem("saved", true);
+	},
+	
+	load: function() {
+		this.player.hp = parseInt(this.getItem("hp"));
+		this.player.name = this.getItem("name");
+	},
+	
+	setItem: function(key, value) {
+		localStorage.setItem(key, value);
+	},
+	
+	getItem: function(key) {
+		return localStorage.getItem(key);
+	},
+};
