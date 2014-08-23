f8015adb121efb1014b792fbf4cc25514141f16e
Eli <coloredflare@gmail.com>
2014-08-23 14:26:59 +0300

    Show some missions. You can do them but you get nothing.

--- script.js
+++ script.js
@@ -15,6 +15,39 @@ var ImaginaryWorld = {
 			this.load();
 		}
 		this.savingLoop();
+		this.updateVariables();
+		this.updateMissions();
+	},
+	
+	updateVariables: function() {
+		$("#name").text(this.player.name);
+		$("#hp").text(this.player.hp);
+	},
+	
+	updateMissions: function() {
+		$day = $("#day");
+		$night = $("#night");
+		$day.html("");
+		$night.html("");
+		
+		$.each(this.day.missions, function(i, mission) {
+			var string = "<h4>"+mission.title+"</h4>";
+			string += "<h5>"+mission.description+"</h5>";
+			string += "<button onclick=\"ImaginaryWorld.missionDone(\'" + mission.id + "\')\">Do mission</button>";
+			$day.html($day.html() + string);
+		});
+		
+		$.each(this.night.missions, function(i, mission) {
+			var string = "<h4>"+mission.title+"</h4>";
+			string += "<h5>"+mission.description+"</h5>";
+			string += "<button onclick=\"ImaginaryWorld.missionDone(\'" + mission.id + "\')\">Do mission</button>";
+			$night.html($night.html() + string);
+		});
+		
+	},
+	
+	missionDone: function(id) {
+		
 	},
 	
 	save: function() {
@@ -35,4 +68,17 @@ var ImaginaryWorld = {
 	getItem: function(key) {
 		return localStorage.getItem(key);
 	},
-};
+	
+	day: {
+		missions: [
+			{id: 1, title: "Eat", description: "Eat somethign to energize yourself.", difficulty: 2},
+			{id: 2, title: "Workout", description: "Train your muscles.", difficulty: 1}
+		]
+	},
+	
+	night: {
+		missions: [
+			{id: 1, title: "Kill dragons", description: "Kill some dragons to raise your experience.", difficulty: 2},
+			{id: 2, title: "Make campfire", description: "Sit by the fire.", difficulty: 1}
+		]
+	}};
