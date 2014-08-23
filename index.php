110db862842bcc0e8a350a0633c90348c99865ca
Eli <coloredflare@gmail.com>
2014-08-23 22:10:56 +0300

    Awesome super responsive header made with flex

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta name="viewport" content="width=device-width">
+		
 		<title>Imaginary World</title>
 		<link rel="stylesheet" type="text/css" href="theme.css" />
 		
@@ -16,15 +18,26 @@
 	</head>
 
 	<body class="day" onload="ImaginaryWorld.start()">
-		<h1>Imaginary World</h1>
-		<h3>An awesome game</h3>
-		<h3>
-			Welcome back, <span id="name" onblur="ImaginaryWorld.updateName()" contenteditable></span>! 
-			You have <span id="hp"></span> hp and <span id="xp"></span> xp.
-			You play in <span id="time"></span> mode.
-			<button onclick="ImaginaryWorld.changeMode()">Change mode now</button>
-		</h3>
-		
+		<div class="header">
+			<h1>Imaginary World</h1>
+			<h3>Howdy, <span id="name" onblur="ImaginaryWorld.updateName()" contenteditable></span>!</h3>
+		</div>
+		<div>
+			<h3>An awesome game</h3>
+			<h3>
+				You have
+				<span id="hp"></span>
+				hp and
+				<span id="xp"></span>
+				xp. You play in
+				<span id="time"></span>
+				mode.
+				<button onclick="ImaginaryWorld.changeMode()">
+					Change mode now
+				</button>
+			</h3>
+		</div>
+
 		<div id="day">
 			<h2>Day Missions</h2>
 			<? foreach ($day_missions as $mission) { ?>
