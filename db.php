cd0399e42a61a9df0ec4e573b8b3c34a1c54c83b
Eli <coloredflare@gmail.com>
2014-08-23 17:25:40 +0300

    Forgot to change mission id when I copy pasted.

--- db.php
+++ db.php
@@ -20,4 +20,4 @@ $day_missions[] = new Mission(2, "Workout", "Train your muscles.", 1);
 
 $night_missions = [];
 $night_missions[] = new Mission(1, "Hunt dragons", "Kill some dragons to raise your experience.", 2);
-$night_missions[] = new Mission(1, "Campfire", "Sit by the fire.", 1);
+$night_missions[] = new Mission(2, "Campfire", "Sit by the fire.", 1);
