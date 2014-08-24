12c800f13c76ec309ed6c02b85a9f38ca00919f7
Eli <coloredflare@gmail.com>
2014-08-24 13:18:22 +0300

    Add some missions. I goat to add more.

--- db.php
+++ db.php
@@ -15,8 +15,10 @@ class Mission {
 }	
 
 $day_missions = [];
-$day_missions[] = new Mission(1, "Eat", "Eat somethign to energize yourself.", 2);
-$day_missions[] = new Mission(2, "Workout", "Train your muscles.", 1);
+$day_missions[] = new Mission(1, "Eat", "Eat something to energize yourself.", 2);
+$day_missions[] = new Mission(2, "Workout", "Train your muscles.", 4);
+$day_missions[] = new Mission(3, "Wash dishes", "You do not have a choice, but to wash them.", 5);
+$day_missions[] = new Mission(4, "Take a nap", "You always have time for a nap.", 1);
 
 $night_missions = [];
 $night_missions[] = new Mission(1, "Hunt dragons", "Kill some dragons to greatly raise your experience.", 100);
