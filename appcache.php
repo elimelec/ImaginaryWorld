4d0fc9c82d9e98f0eb4e1fe3c9830678ae090ea2
Eli <coloredflare@gmail.com>
2014-08-24 22:39:32 +0300

    Hope it's working

---
+++ appcache.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: text/cache-manifest');
+?>
+CACHE MANIFEST
+# 201408242236
+
+# Explicitly cached entries
+/
+index.php
+kings.php
+theme.css
+script.php
+//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
