1c5349e834c2fb4331f462c581f5691e387add39
Eli <coloredflare@gmail.com>
2014-08-24 20:08:47 +0300

    Almost nothing

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
 <?php 
 	include 'missions.php';
-
-
 ?>
-
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width">
-		
 		<title>Imaginary World</title>
 		<link rel="stylesheet" type="text/css" href="theme.css" />
-		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" async></script>
 		<script type="text/javascript" src="script.php" async></script>
-		
 	</head>
-
 	<body class="day" onload="ImaginaryWorld.start()">
 		<div class="header">
 			<h1>Imaginary World</h1>
 			<h3>Howdy, <span id="name" onblur="ImaginaryWorld.updateName()" contenteditable></span></h3>
 		</div>
-		
 		<div class="stats">
 			<h3>HP: <span id="hp"></span></h3>
 			<h3>XP: <span id="xp"></span></h3>
 			<h3>You play in <span id="time"></span> mode</h3>
 			<h3><button onclick="ImaginaryWorld.changeMode()">Change game mode</button></h3>
 		</div>
-	
 		<div class="missions_area">
 			<div id="day">
 				<h2>Day Missions</h2>
@@ -43,7 +34,6 @@
 					<? } ?>
 				</div>
 			</div>
-			
 			<div id="night">
 				<h2>Night Missions</h2>
 				<div class="missions_container">
@@ -56,7 +46,6 @@
 					<? } ?>
 				</div>
 			</div>
-			
 		</div>
 	</body>
 </html>
