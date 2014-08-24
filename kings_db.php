0aee3f8b85c18c77e4787667b5c1aa20a04dfbd2
Eli <coloredflare@gmail.com>
2014-08-24 20:48:36 +0300

    Add two kings

--- kings_db.php
+++ kings_db.php
@@ -16,20 +16,12 @@ class King {
 
 $day_kings = [];
 $i = 0;
-$day_kings[] = new King($i++, "Eat", "Eat something to energize yourself.", 2);
-$day_kings[] = new King($i++, "Workout", "Train your muscles.", 4);
-$day_kings[] = new King($i++, "Wash dishes", "You do not have a choice, but to wash them.", 5);
-$day_kings[] = new King($i++, "Take a nap", "You always have time for a nap.", 1);
-$day_kings[] = new King($i++, "Make pizza", "If you do not have pizza, you make pizza.", 2);
-$day_kings[] = new King($i++, "Go shopping", "Go to buy ingredients for pizza.", 3);
-$day_kings[] = new King($i++, "LudumDare", "Participate in Ludum Dare.", 6);
-$day_kings[] = new King($i++, "Facebook", "Like some random photos.", 1);
+$day_kings[] = new King($i++, "Mosquito", "Kill the mosquito king.", 1000);
+$day_kings[] = new King($i++, "Dog", "Kill that annoying barking dog king.", 5000);
 
 
 $i = 0;
 $night_kings = [];
-$night_kings[] = new King($i++, "Hunt dragons", "Kill some dragons to greatly raise your experience.", 500);
-$night_kings[] = new King($i++, "Campfire", "Sit by the fire.", 50);
-$night_kings[] = new King($i++, "Walk you dog", "Did you ever walked your dog at night? Yes, now", 50);
-$night_kings[] = new King($i++, "Go fishing", "You never went for fishingat night. You do now.", 20);
-$night_kings[] = new King($i++, "Gold", "Go find some pure gold.", 100);
+$night_kings[] = new King($i++, "Hunt dragons", "Kill some dragons to greatly raise your experience.", 3000);
+$night_kings[] = new King($i++, "Dragon", "Who does not want the head of the dragon king?", 100000);
+
